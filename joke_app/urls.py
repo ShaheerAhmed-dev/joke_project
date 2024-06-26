8b8c64c6470d020af705e0f555d91c80aedfbfa0
shery-coder <shaheerahmed49@gmail.com>
2024-06-26 19:58:27 +0500

    implementation of joke api and mofications on the front-end part of the site

--- joke_app/urls.py
+++ joke_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import SignUpView, UserLoginView, logout_user, HomeView
+from .views import SignUpView, UserLoginView, logout_user, HomeView, NewJokeView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
     path('home/', HomeView.as_view(), name='home'),
+    path('new_joke/', NewJokeView.as_view(), name='new_joke'),
 ]
--- joke_app/views.py
+++ joke_app/views.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from django.views.generic.edit import FormView
 from django.views.generic import TemplateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login,authenticate, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+import requests
+
 
 class SignUpView(FormView):
     template_name = 'joke_app/signup.html'
@@ -20,9 +22,15 @@ class SignUpView(FormView):
         login(self.request, user)
         return super().form_valid(form)
     
+    def get_success_url(self):
+        return reverse_lazy('login')
+    
 class UserLoginView(LoginView):
     template_name = 'joke_app/login.html'
-    success_url='home'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
 
 def logout_user(request):
     logout(request)
@@ -30,3 +38,22 @@ def logout_user(request):
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'joke_app/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        joke = self.request.session.get('joke')
+        if not joke:
+            response = requests.get('https://official-joke-api.appspot.com/random_joke')
+            joke = response.json()
+            self.request.session['joke'] = joke
+        context['joke'] = joke
+        return context
+    
+class NewJokeView(LoginRequiredMixin, TemplateView):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        response = requests.get('https://official-joke-api.appspot.com/random_joke')
+        joke = response.json()
+        request.session['joke'] = joke
+        return redirect('/jokeapp/home')
